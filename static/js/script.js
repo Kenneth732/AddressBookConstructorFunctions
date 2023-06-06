99eed42282a7ade934fe797ca76ceff69db2a22f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-06 16:03:47 +0300

    Address_proto...

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,21 @@
 // Business Logic for AddressBook ---------
 function AddressBook() {
     this.contacts = {};
-  }
-  
-  AddressBook.prototype.addContact = function(contact) {
-    this.contacts[contact.firstName] = contact;
-  };
-  
+}
+
+AddressBook.prototype.addContact = function(contact) {
+    return this.contacts[contact.firstName] = contact;
+};
+
   // Business Logic for Contacts ---------
-  function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
-  }
+}
   
-  Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
-  };
+};
+
+
